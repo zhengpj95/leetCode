5ef834dee2e9b8a699ec9971deed71d5b931931d
ZhengAu <598359997@qq.com>
2020-06-08 19:08:54 +0800

    add

---
+++ hard/4.medianOfTwoSortedArrays.js
@@ -0,0 +1,26 @@
+/**
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ * You may assume nums1 and nums2 cannot be both empty.
+ */
+
+/**
+ * Time complexity: O(m+n)
+ * Space complexity: O(m+n)
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+	let arr = nums1.concat(nums2).sort((a, b) => a - b);
+	let mid = Math.floor(arr.length / 2);
+
+	console.log(arr, mid);
+	if (arr.length & (1 === 1)) {
+		return arr[mid].toFixed(1);
+	} else {
+		return ((arr[mid] + arr[mid - 1]) / 2).toFixed(1);
+	}
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
