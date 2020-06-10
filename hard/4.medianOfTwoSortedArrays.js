5017074851e61dd5553a605e8b9fec0126093389
ZhengAu <598359997@qq.com>
2020-06-10 12:54:04 +0800

    update

--- hard/4.medianOfTwoSortedArrays.js
+++ hard/4.medianOfTwoSortedArrays.js
@@ -14,13 +14,14 @@
 var findMedianSortedArrays = function (nums1, nums2) {
 	let arr = nums1.concat(nums2).sort((a, b) => a - b);
 	let mid = Math.floor(arr.length / 2);
+	// console.log(arr, mid);
 
-	console.log(arr, mid);
 	if (arr.length & (1 === 1)) {
-		return arr[mid].toFixed(1);
+		return arr[mid] * 1.0;
 	} else {
-		return ((arr[mid] + arr[mid - 1]) / 2).toFixed(1);
+		return ((arr[mid] + arr[mid - 1]) / 2) * 1.0;
 	}
 };
 
 console.log(findMedianSortedArrays([1, 3], [2]));
+console.log(findMedianSortedArrays([1, 3], [2, 4]));
