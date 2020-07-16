6bc8b2ca34c4f0f2bf5b3c64413879e3f6ee0361
ZhengAu <598359997@qq.com>
2020-07-16 09:55:35 +0800

    add postorder traversal

---
+++ medium/145.binaryTreePostorderTraversal.js
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+	constructor(val, left, right) {
+		this.val = !val ? 0 : val;
+		this.left = !left ? null : left;
+		this.right = !right ? null : right;
+	}
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+	let res = [];
+	let traversal = (root) => {
+		if (!root) {
+			return;
+		}
+		traversal(root.left);
+		traversal(root.right);
+		res.push(root.val);
+	};
+	traversal(root);
+	return res;
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal2 = function (root) {
+	let res = [];
+	let stack = [];
+	let curNode = root;
+
+	while (curNode) {
+		res.unshift(curNode.val);
+		if (curNode.left) {
+			stack.push(curNode.left);
+		}
+		if (curNode.right) {
+			stack.push(curNode.right);
+		}
+		curNode = stack.pop();
+	}
+	return res;
+};
+
+let node5 = new TreeNode(5);
+let node4 = new TreeNode(4);
+let node3 = new TreeNode(3, null, node5);
+let node2 = new TreeNode(2, node3, node4);
+let node1 = new TreeNode(1, null, node2);
+
+console.log(postorderTraversal(node1));
+console.log(postorderTraversal2(node1));
