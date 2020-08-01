7be46f9cd10bc708ef7ffe7eac8e8af922a5c878
ZhengAu <598359997@qq.com>
2020-08-01 12:28:01 +0800

    前序遍历添加morris遍历

--- medium/144.binaryTreePreorderTraversal.js
+++ medium/144.binaryTreePreorderTraversal.js
@@ -44,6 +44,34 @@ const preorderTraversal2 = function (root) {
 	return res;
 };
 
+/**
+ * Morris Traversal
+ * @param {TreeNode} root
+ * @returns {number[]}
+ */
+const morrisTraversal = function (root) {
+	let res = [];
+	let curNode = root;
+	while (curNode) {
+		res.push(curNode.val);
+		if (!curNode.left) {
+			curNode = curNode.right;
+		} else {
+			// 找到左子树中最右子节点
+			let preNode = curNode.left;
+			while (preNode.right) {
+				preNode = preNode.right;
+			}
+			// 此子节点的右节点设置为curNode.right，因为curNode已经输出了
+			preNode.right = curNode.right;
+			let tempNode = curNode;
+			curNode = curNode.left;
+			tempNode.left = null;
+		}
+	}
+	return res;
+};
+
 let node5 = new TreeNode(5);
 let node4 = new TreeNode(4);
 let node3 = new TreeNode(3, null, node5);
@@ -52,3 +80,4 @@ let node1 = new TreeNode(1, null, node2);
 
 console.log(preorderTraversal(node1));
 console.log(preorderTraversal2(node1));
+console.log(morrisTraversal(node1));
