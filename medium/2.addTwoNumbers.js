acd56291b38c561a297571d9588c0f91cc41bf53
ZhengAu <598359997@qq.com>
2020-09-07 11:35:32 +0800

    添加新的方法

--- medium/2.addTwoNumbers.js
+++ medium/2.addTwoNumbers.js
@@ -40,6 +40,63 @@ var addTwoNumbers = function (l1, l2) {
 };
 
 /**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers2 = function (l1, l2) {
+	let result = null;
+	let addValue = 0;
+	let newValue = 0;
+	while (l1 || l2) {
+		newValue = (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+		newValue += addValue;
+
+		addValue = Math.floor(newValue / 10);
+		newValue = newValue % 10;
+		result = insertTail(result, newValue);
+
+		l1 = l1 ? l1.next : null;
+		l2 = l2 ? l2.next : null;
+	}
+	if (addValue) {
+		result = insertTail(result, addValue);
+	}
+
+	return result;
+};
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers3 = function (l1, l2) {
+	let addValue = 0;
+	let newValue = 0;
+	let list = new ListNode();
+	let node = list;
+
+	while (l1 || l2) {
+		newValue = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + addValue;
+
+		addValue = Math.floor(newValue / 10);
+		newValue = newValue % 10;
+
+		node.next = new ListNode(newValue);
+		node = node.next;
+
+		l1 = l1 ? l1.next : null;
+		l2 = l2 ? l2.next : null;
+	}
+	if (addValue) {
+		node.next = new ListNode(addValue);
+	}
+
+	return list.next;
+};
+
+/**
  * @param {ListNode} list
  * @param {number} val
  */
