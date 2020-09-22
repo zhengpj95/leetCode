beeeb9207c1a9a1c4d6b3cec7e4a2966dfd80a0a
ZhengAu <598359997@qq.com>
2020-09-22 19:06:43 +0800

    由前序和中序得到二叉树

---
+++ medium/105.constructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -0,0 +1,45 @@
+/**
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ */
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+	this.val = val === undefined ? 0 : val;
+	this.left = left === undefined ? null : left;
+	this.right = right === undefined ? null : right;
+}
+/**
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+	let build = function (preorder, preStart, preEnd, inorder, inStart, inEnd) {
+		if (preStart > preEnd) {
+			return null;
+		}
+		let rootValue = preorder[preStart]; //前序的第一个就是根结点
+		let index = 0; //中序中找出上面找到的根结点的位置，此位置左边是左子树，右边是右子树
+		for (let i = inStart; i <= inEnd; i++) {
+			if (inorder[i] == rootValue) {
+				index = i;
+				break;
+			}
+		}
+
+		let root = new TreeNode(rootValue); //根结点
+		root.left = build(preorder, preStart + 1, preStart + (index - inStart), inorder, inStart, index - 1); //左子树
+		root.right = build(preorder, preStart + (index - inStart) + 1, preEnd, inorder, index + 1, inEnd); //右子树
+		return root;
+	};
+
+	let root = build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+	return root;
+};
+
+let preArr = [3, 9, 20, 15, 7];
+let inArr = [9, 3, 15, 20, 7];
+
+console.log(buildTree(preArr, inArr));
