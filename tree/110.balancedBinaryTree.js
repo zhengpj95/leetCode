0734aeaf53bf0f51afbd1691a42932ff9c1c8375
ZhengAu <598359997@qq.com>
2020-09-24 11:40:10 +0800

    判断树是否平衡方法2

--- tree/110.balancedBinaryTree.js
+++ tree/110.balancedBinaryTree.js
@@ -8,6 +8,8 @@
 const { TreeNode, createTree } = require('./TreeNode');
 
 /**
+ * Time complexity: O(nlogn) 遍历n个结点，遍历每个结点时都要遍历其子树
+ * Space complexity: O(n)
  * @param {TreeNode} root
  * @return {boolean}
  */
@@ -23,7 +25,29 @@ const isBalanced = function (root) {
 	return Math.abs(leftH - rightH) <= 1 && isBalanced(root.left) && isBalanced(root.right);
 };
 
+/**
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isBalanced2 = function (root) {
+	let checkHeight = function (root) {
+		if (!root) return 0;
+
+		let leftH = checkHeight(root.left);
+		if (leftH == -1) return -1;
+		let rightH = checkHeight(root.right);
+		if (rightH == -1) return -1;
+		if (Math.abs(leftH - rightH) > 1) return -1;
+		return Math.max(leftH, rightH) + 1;
+	};
+
+	return checkHeight(root) != -1;
+};
+
 let arr = [3, 9, 20, null, null, 15, 7];
 let root = createTree(arr);
 
 console.log(isBalanced(root));
+console.log(isBalanced2(root));
