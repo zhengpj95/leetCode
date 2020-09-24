465574656e4137b2c3e51e93f63dc3e3a6fdb328
ZhengAu <598359997@qq.com>
2020-09-24 19:55:05 +0800

    路径和等于给定值的方法2

--- tree/113.pathSum2.js
+++ tree/113.pathSum2.js
@@ -14,7 +14,7 @@ const { createTree } = require('./TreeNode');
  * @param {number} sum
  * @return {number[][]}
  */
-var pathSum = function (root, sum) {
+const pathSum = function (root, sum) {
 	if (!root) return [];
 	let result = [];
 
@@ -36,7 +36,51 @@ var pathSum = function (root, sum) {
 	return result;
 };
 
+/**
+ * Approach 2: Queue
+ * Time complexity: O(n)
+ * @param {Treenode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+const pathSum2 = function (root, sum) {
+	if (!root) return [];
+	let result = [];
+
+	// 累计路径和等于给定值
+	// let list = [[root, root.val, [root.val]]];
+	// while (list.length) {
+	// 	let [node, val, arr] = list.shift();
+	// 	if (!node.left && !node.right && sum == val) {
+	// 		result.push(arr);
+	// 	}
+	// 	if (node.left) {
+	// 		list.push([node.left, val + node.left.val, [...arr, node.left.val]]);
+	// 	}
+	// 	if (node.right) {
+	// 		list.push([node.right, val + node.right.val, [...arr, node.right.val]]);
+	// 	}
+	// }
+
+	// 累计减去路径的结点值等于0
+	let list = [[root, sum - root.val, [root.val]]];
+	while (list.length) {
+		let [node, val, arr] = list.shift();
+		if (!node.left && !node.right && val == 0) {
+			result.push(arr);
+		}
+		if (node.left) {
+			list.push([node.left, val - node.left.val, [...arr, node.left.val]]);
+		}
+		if (node.right) {
+			list.push([node.right, val - node.right.val, [...arr, node.right.val]]);
+		}
+	}
+	return result;
+};
+
 let arr = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1];
 let root = createTree(arr);
 
 console.log(pathSum(root, 22));
+console.log(pathSum2(root, 22));
