7183d1460186c3c7a0a83cded94bfd67bd5dc32d
ZhengAu <598359997@qq.com>
2020-09-26 11:26:36 +0800

    全部路径的结点值之和

---
+++ tree/129.sumRoottoLeafNumbers.js
@@ -0,0 +1,44 @@
+/**
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+ * An example is the root-to-leaf path 1->2->3 which represents the number 123.
+ * Find the total sum of all root-to-leaf numbers.
+ * @author zheng
+ * @date 2020/09/26
+ */
+
+const { TreeNode, createTree } = require('./TreeNode');
+
+/**
+ * Time complexity: O(n)
+ * Space complexity: O(路经数目+最多结点层的结点数)?? 约等于O(n)??
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumNumbers = function (root) {
+	if (!root) return 0;
+	let list = [[root, []]];
+	let result = [];
+	while (list.length) {
+		let [node, arr] = list.shift();
+		let newArr = [...arr, node.val];
+		if (!node.left && !node.right) {
+			result.push(newArr);
+		}
+		if (node.left) {
+			list.push([node.left, newArr]);
+		}
+		if (node.right) {
+			list.push([node.right, newArr]);
+		}
+	}
+
+	let total = 0;
+	for (let item of result) {
+		total += parseInt(item.toString().replace(/,/gi, ''));
+	}
+	return total;
+};
+
+let arr = [4, 9, 0, 5, 1];
+let root = createTree(arr);
+console.log(sumNumbers(root));
