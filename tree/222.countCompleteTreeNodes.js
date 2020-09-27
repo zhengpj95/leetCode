bd0c9daa3b4d051309020ed87a715ed6cd03d84d
ZhengAu <598359997@qq.com>
2020-09-27 20:35:27 +0800

    计算完全二叉树结点数方法

--- tree/222.countCompleteTreeNodes.js
+++ tree/222.countCompleteTreeNodes.js
@@ -4,7 +4,7 @@
  * @date 2020/09/27 10:25:18
  */
 
-const { TreeNode, createTree } = require('./TreeNode');
+const { TreeNode, createTree, getTreeHeight } = require('./TreeNode');
 
 /**
  * Time complexity: O(n)
@@ -34,6 +34,69 @@ const countNodes = function (root) {
 	return result;
 };
 
-let arr = [1, 2, 3, 4, 5, 6];
+/**
+ * Time complexity: O(logn)
+ * Space complexity: O(1)
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const countNodes2 = function (root) {
+	let total = 0;
+	let h = getTreeHeight(root) - 1; //顶点层为0开始算
+
+	while (root) {
+		if (getTreeHeight(root.right) - 1 == h - 1) {
+			total += 1 << h;
+			root = root.right;
+		} else {
+			total += 1 << (h - 1);
+			root = root.left;
+		}
+		// console.log('nums = ', total);
+		h--;
+	}
+	return total;
+};
+
+/**
+ * Time complexity: O(logn * logn)  【logn约为树高，logn向下取整+1，或者log(n+1)向上取整】递归logn次，每次递归计算子树高又为logn
+ * Space complexity: O(logn)
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const countNodes3 = function (root) {
+	let leftDepth = function (root) {
+		let depth = 0;
+		while (root) {
+			depth++;
+			root = root.left;
+		}
+		return depth;
+	};
+	let rightDepth = function (root) {
+		let depth = 0;
+		while (root) {
+			depth++;
+			root = root.right;
+		}
+		return depth;
+	};
+
+	let leftH = leftDepth(root);
+	let rightH = rightDepth(root);
+
+	// console.log('leftH = %s, rightH = %s', leftH, rightH);
+	// 若是满二叉树就返回结果
+	if (leftH === rightH) {
+		return (1 << leftH) - 1;
+	}
+	return 1 + countNodes3(root.left) + countNodes3(root.right);
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 let root = createTree(arr);
 console.log(countNodes(root));
+// console.log(`==========================`);
+console.log(countNodes2(root));
+// console.log(`==========================`);
+console.log(countNodes3(root));
