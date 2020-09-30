05e89d0d1f1e2db08e0d8acf7e3fcfb56b2d45e6
ZhengAu <598359997@qq.com>
2020-09-30 15:15:01 +0800

    左叶子结点之和方法3

--- tree/404.sumofLeftLeaves.js
+++ tree/404.sumofLeftLeaves.js
@@ -49,9 +49,40 @@ const sumOfLeftLeaves2 = function (root) {
 	return findLeft(root, false);
 };
 
+/**
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves3 = function (root) {
+	if (!root) return 0;
+	let sum = 0;
+	let queue = [root];
+
+	while (queue.length) {
+		let node = queue.shift();
+		if (node.left) {
+			if (!node.left.left && !node.left.right) {
+				sum += node.left.val;
+			} else {
+				queue.push(node.left);
+			}
+		}
+		if (node.right) {
+			// 判断是否是非叶子结点再入队，若是右叶子结点则无需多操作
+			if (node.right.left || node.right.right) {
+				queue.push(node.right);
+			}
+		}
+	}
+	return sum;
+};
+
 let arr = [3, 9, 20, null, null, 15, 7];
 let root = createTree(arr);
 console.log(sumOfLeftLeaves(root));
 console.log(sumOfLeftLeaves(createTree([1]))); // 输出0
 
 console.log(sumOfLeftLeaves2(root));
+console.log(sumOfLeftLeaves3(root));
