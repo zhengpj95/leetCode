eec6e40ee0d90646c300803ce290f68d5b5d1466
ZhengAu <598359997@qq.com>
2020-10-09 16:24:22 +0800

    路径和（todo）

--- tree/437.pathSum3.js
+++ tree/437.pathSum3.js
@@ -75,7 +75,71 @@ const pathSum2 = function (root, sum) {
 	return total;
 };
 
+/**
+ * DFS
+ * Time complexity: O(N^2) ??
+ * Space complexity: O(N) ??
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+const pathSum3 = function (root, sum) {
+	if (!root) return 0;
+
+	let dfs = (root, sum) => {
+		if (!root) return 0;
+
+		let total = 0;
+		if (sum === root.val) {
+			total++;
+		}
+
+		total += dfs(root.left, sum - root.val);
+		total += dfs(root.right, sum - root.val);
+		return total;
+	};
+
+	return dfs(root, sum) + pathSum3(root.left, sum) + pathSum3(root.right, sum);
+};
+
+/**
+ * Time complexity:
+ * Space complexity:
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+const pathSum4 = function (root, sum) {
+	if (!root) return 0;
+	let total = 0;
+	let map = new Map();
+
+	let helper = (root, currSum, targetSum, map) => {
+		console.log(map);
+		if (!root) return;
+		currSum += root.val;
+		if (map.has(currSum - targetSum)) {
+			total += map.get(currSum - targetSum);
+		}
+
+		if (!map.has(currSum)) {
+			map.set(currSum, 1);
+		} else {
+			map.set(currSum, map.get(currSum) + 1);
+		}
+		helper(root.left, currSum, targetSum, map);
+		helper(root.right, currSum, targetSum, map);
+		map.set(currSum, map.get(currSum) - 1);
+	};
+
+	helper(root, 0, sum, map);
+	return total;
+};
+
 let arr = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1];
 let root = createTree(arr);
-console.log(pathSum(root, 8));
-console.log(pathSum2(root, 1));
+// console.log(pathSum(root, 8));
+// console.log(pathSum2(root, 8));
+// console.log(pathSum3(root, 8));
+console.log(pathSum4(root, 8));
+// console.log(pathSum4(root, 1));
