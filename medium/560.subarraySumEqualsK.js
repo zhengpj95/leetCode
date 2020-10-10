33644c1847e34a1c57fd150922a978b168ac015f
ZhengAu <598359997@qq.com>
2020-10-10 20:27:42 +0800

    子数组和--前缀和技巧+缓存，O(N)时间复杂度

--- medium/560.subarraySumEqualsK.js
+++ medium/560.subarraySumEqualsK.js
@@ -6,6 +6,7 @@
  */
 
 /**
+ * 前缀和技巧
  * Time complexity: O(N^2)
  * Space complexity: O(N)
  * @param {number[]} nums
@@ -29,5 +30,34 @@ const subarraySum = function (nums, k) {
 	return result;
 };
 
+/**
+ * 前缀和技巧 + 缓存
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraySum2 = function (nums, k) {
+	let map = new Map();
+	map.set(0, 1);
+
+	let result = 0;
+	let preSum = 0; //前n项和
+	for (let i = 0; i < nums.length; i++) {
+		preSum += nums[i];
+
+		// let target = preSum - k;
+		// if (map.has(target)) {
+		// 	result += map.get(target);
+		// }
+		result += map.get(preSum - k) ? map.get(preSum - k) : 0;
+
+		map.set(preSum, map.has(preSum) ? map.get(preSum) + 1 : 1);
+	}
+	return result;
+};
+
 let arr = [1, 1, 1, 3, 2];
 console.log(subarraySum(arr, 2));
+console.log(subarraySum2(arr, 2));
