adffdf73009500fd8639319c954a6dd5f2894fdc
ZhengAu <598359997@qq.com>
2020-10-10 11:33:10 +0800

    删除结点（bug）

---
+++ tree/450.deleteNodeInaBST.js
@@ -0,0 +1,66 @@
+/**
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST.
+ * Return the root node reference (possibly updated) of the BST.
+ *
+ * Basically, the deletion can be divided into two stages:
+ * 1. Search for a node to remove.
+ * 2. If the node is found, delete the node.
+ *
+ * Follow up: Can you solve it with time complexity O(height of tree)?
+ * @author zheng
+ * @date 2020/10/10 10:32:10
+ */
+
+const { TreeNode, createTree } = require('./TreeNode');
+
+/**
+ * 先找到要删除的结点，然后又找到要替换到此结点位置的结点，然后替换两结点值，这样结点联系就断开了，删除结点就麻烦了
+ * 这样子的思路有很大的问题，BUG
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+const deleteNode = function (root, key) {
+	if (!root) return null;
+	if (key === undefined || key === null) return root;
+
+	let findNode = (root, key) => {
+		if (!root) return null;
+		if (root.val === key) return root;
+		if (key < root.val) return findNode(root.left, key);
+		if (key > root.val) return findNode(root.right, key);
+	};
+
+	let findRight = (root) => {
+		if (!root) return null;
+		if (root.right) return findRight(root.right);
+		return root;
+	};
+
+	let findLeft = (root) => {
+		if (!root) return null;
+		if (root.left) return findLeft(root.left);
+		return root;
+	};
+
+	let findReplace = (root) => {
+		if (!root) return null;
+		if (root.left) return findRight(root.left);
+		else return findLeft(root.right);
+	};
+
+	let node = findNode(root, key);
+	if (!node) return null;
+	let replaceNode = findReplace(node);
+
+	let tmp = node.val;
+	node.val = replaceNode.val;
+	replaceNode.val = tmp;
+
+	return root;
+};
+
+let arr = [5, 3, 6, 2, 4, null, 7];
+let root = createTree(arr);
+
+deleteNode(root, 3);
