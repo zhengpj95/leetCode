d65d4dccc0db07d3dd297b766df3dcece22aaa7a
ZhengAu <598359997@qq.com>
2020-10-11 20:30:06 +0800

    排序数组找指定数值下标--二分法

--- binarySearch/33.searchinRotatedSortedArray.js
+++ binarySearch/33.searchinRotatedSortedArray.js
@@ -26,8 +26,62 @@ const bruteSearch = function (nums, target) {
 };
 
 /**
+ * 未曾很理解 (todo)
+ * Time complexity: O(logN)
+ * Space complexity: O(1)
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-const binarySearch = function (nums, target) {};
+const binarySearch = function (nums, target) {
+	let low = 0;
+	let high = nums.length - 1;
+	// 找出数组中最小数索引
+	while (low < high) {
+		let mid = Math.floor((low + high) / 2);
+		if (nums[mid] > nums[high]) low = mid + 1;
+		else high = mid;
+	}
+
+	let rotate = low; //旋转点
+	low = 0;
+	high = nums.length - 1;
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+		let realMid = (mid + rotate) % nums.length;
+		if (nums[realMid] === target) return realMid;
+		if (nums[realMid] < target) low = mid + 1;
+		else high = mid - 1;
+	}
+	return -1;
+};
+
+/**
+ * Time complexity: O(logN)
+ * Space complexity: O(1)
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const binarySearch2 = function (nums, target) {
+	let low = 0,
+		high = nums.length - 1;
+
+	while (low <= high) {
+		let mid = low + Math.floor((high - low) / 2);
+		if (nums[mid] === target) return mid;
+		// 右边有序
+		if (nums[mid] < nums[high]) {
+			if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+			else high = mid - 1;
+		} else {
+			if (nums[low] <= target && target < nums[mid]) high = mid - 1;
+			else low = mid + 1;
+		}
+	}
+	return -1;
+};
+
+let arr = [3, 4, 1, 2]; // [4, 5, 6, 7, 8, 9, 10, 0, 1, 2, 3];
+console.log(binarySearch(arr, 3));
+console.log(binarySearch2(arr, 3));
