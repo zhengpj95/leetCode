60719c720aed86e148cdf51384271ef07ca02d6b
ZhengAu <598359997@qq.com>
2020-10-11 14:39:18 +0800

    链表基础文件

---
+++ linkedList/ListNode.js
@@ -0,0 +1,27 @@
+/**
+ * @param {any} val
+ * @param {any} next
+ */
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val;
+	this.next = next === undefined ? null : next;
+}
+
+/**
+ * 创建单链表
+ * @param {any[]} list
+ */
+function createList(list) {
+	if (!list || !list.length) return null;
+	let head = new ListNode();
+	let currNode = head;
+	for (let ele of list) {
+		let node = new ListNode(ele);
+		currNode.next = node;
+		currNode = currNode.next;
+	}
+	return head;
+}
+
+module.exports.ListNode = ListNode;
+module.exports.createList = createList;
