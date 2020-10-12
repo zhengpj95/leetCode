d0df8b81f4b4faa665e64327b9b2338936529c3f
ZhengAu <598359997@qq.com>
2020-10-12 20:03:26 +0800

    移除给定元素--双指针

--- twoPointers/27.removeElement.js
+++ twoPointers/27.removeElement.js
@@ -15,3 +15,26 @@ var removeElement = function (nums, val) {
 	}
 	return nums.length;
 };
+
+/**
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement2 = function (nums, val) {
+	if (!nums || !nums.length) return 0;
+	let slow = 0,
+		fast = 0;
+	while (fast < nums.length) {
+		if (nums[fast] != val) {
+			nums[slow++] = nums[fast];
+		}
+		fast++;
+	}
+	return slow;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement2([3, 2, 4, 5, 2, 8, 3], 3));
