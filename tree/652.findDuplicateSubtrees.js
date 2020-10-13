5489f4929052aeb442c6300a0f518b065eb907a2
ZhengAu <598359997@qq.com>
2020-10-13 09:57:21 +0800

    判断是否有相同的子树--修改时空复杂度

--- tree/652.findDuplicateSubtrees.js
+++ tree/652.findDuplicateSubtrees.js
@@ -11,14 +11,16 @@
  * 很显然，这里主要判断是否有子树相同，那么就是先得到左右子树，再去判断，那就是后序遍历
  * 而且你也不能得到子树后，再依次判断每个节点的结果以及值是否相同，那么这里使用 序列化 的方式来判断
  *
+ * 当然这道题使用前序遍历或中序遍历也是可以的，主要是判断 序列化 相同即可。但是分析题目的时候要有这种思路
+ *
  * 遇到树的问题，无非就是判断使用什么遍历方式，再在遍历方式中进行题目需求的操作
  */
 
 const { TreeNode, createTree } = require('./TreeNode');
 
 /**
- * Time complexity: O(N)
- * Space complexity: O(N)
+ * Time complexity: O(N^2) 需要遍历每个结点，但是每个结点的序列化也要花费 O(N) 的时间
+ * Space complexity: O(N^2)
  * @param {TreeNode} root
  * @return {TreeNode[]}
  */
