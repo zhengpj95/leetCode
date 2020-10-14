fcd7f668782cbbaea96f9f58819e18b7269309bf
ZhengAu <598359997@qq.com>
2020-10-14 11:21:05 +0800

    链表分成两半且原排序不变--Space complexity:O(1)

--- linkedList/86.partitionList.js
+++ linkedList/86.partitionList.js
@@ -40,10 +40,44 @@ const partition = function (head, x) {
 	return dummy.next;
 };
 
-let arr = [1, 4, 3, 2, 5, 2];
+/**
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+const partition2 = function (head, x) {
+	if (!head || !head.next) return head;
+
+	let dummy = new ListNode(0); //比 x 小的子链表
+	dummy.next = head;
+	let slow = dummy;
+	let fast = dummy;
+
+	let fast1 = new ListNode(-1); //比 x 大的子链表
+	let fast2 = fast1;
+
+	while (fast && fast.next) {
+		if (fast.next.val < x) {
+			slow.next = fast.next;
+			slow = slow.next;
+		} else {
+			fast2.next = fast.next;
+			fast2 = fast2.next;
+		}
+
+		fast = fast.next;
+	}
+	fast2.next = null;
+	slow.next = fast1.next;
+	return dummy.next;
+};
+
+let arr = [1, 4, 3, 2, 4, 5, 2, -1, 0];
 let node = createList(arr);
 
-let res = partition(node.next, 3);
+let res = partition2(node.next, 3);
 while (res) {
 	console.log(res.val);
 	res = res.next;
