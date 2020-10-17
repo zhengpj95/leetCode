aaeffdbcf7bf4e71bb19dcb3693ee653da1a4179
ZhengAu <598359997@qq.com>
2020-10-17 22:56:46 +0800

    无序单链表排序O(logn)空间复杂度

--- linkedList/148.sortList.js
+++ linkedList/148.sortList.js
@@ -21,15 +21,67 @@
  * 基数排序			O(d(n+r)) O(d(n+r)) O(d(n+r))		O(r)			是
  */
 
+/**
+ * https://leetcode.com/problems/sort-list/solution/
+ */
+
 const { ListNode, createList, traversal } = require('./ListNode');
 
 /**
+ * Approach 1: Top Down Merge Sort  ------  Divide and Conquer Strategy
+ * Time complexity: O(nlogn)
+ * Space complxity: O(logn) where n is the number of nodes in linked list.
+ * 					Since the problem is recursive, we need additional space to store the recursive call stack.
+ * 					The maximum depth of the recursion tree is logn
  * @param {ListNode} head
  * @return {ListNode}
  */
 const sortList = function (head) {
-	if (!head) return null;
+	if (!head || !head.next) return head;
+
+	let mid = getListByMid(head);
+	let list1 = sortList(head);
+	let list2 = sortList(mid);
+	return merge(list1, list2);
+};
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ */
+let merge = (list1, list2) => {
 	let dummy = new ListNode(0);
+	let tail = dummy;
+
+	while (list1 && list2) {
+		if (list1.val < list2.val) {
+			tail.next = list1;
+			list1 = list1.next;
+		} else {
+			tail.next = list2;
+			list2 = list2.next;
+		}
+		tail = tail.next;
+	}
+	tail.next = list1 ? list1 : list2;
+	return dummy.next;
+};
+
+/**
+ * 获取中间点后的链表
+ * @param {ListNode} head
+ */
+let getListByMid = (head) => {
+	let prev = null,
+		slow = head,
+		fast = head;
+	while (fast && fast.next) {
+		prev = slow;
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+	prev.next = null;
+	return slow;
 };
 
 let arr = [4, 2, 1, 3]; //[-1, 5, 3, 4, 0];
