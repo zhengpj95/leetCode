565a518ad72246bf69291e190639021edfae698f
ZhengAu <598359997@qq.com>
2020-10-20 19:39:04 +0800

    链表单双结点分开一起--注重命名

--- linkedList/328.oddEvenLinkedList.js
+++ linkedList/328.oddEvenLinkedList.js
@@ -34,6 +34,30 @@ const oddEvenList = function (head) {
 	return head;
 };
 
+/**
+ * 注意命名，学习命名
+ * 变量命名也要讲究，见名知意也是一种能力
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList2 = function (head) {
+	if (!head || !head.next) return head;
+	let odd = head,
+		even = head.next,
+		evenHead = head.next;
+
+	while (even && even.next) {
+		odd.next = even.next;
+		odd = odd.next;
+		even.next = odd.next;
+		even = even.next;
+	}
+	odd.next = evenHead;
+	return head;
+};
+
 let arr = [1, 2, 3, 4, 5, 6];
 let head = createList(arr);
-console.log(traversal(oddEvenList(head.next)));
+console.log(traversal(oddEvenList2(head.next)));
