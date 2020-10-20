df7064e618326be8f3df372d3b9692189c340a61
ZhengAu <598359997@qq.com>
2020-10-20 19:03:56 +0800

    删除结点--给删除结点，不给链表第一个结点

---
+++ linkedList/237.deleteNodeinaLinkedList.js
@@ -0,0 +1,33 @@
+/**
+ * Write a function to delete a node in a singly-linked list.
+ * You will not be given access to the head of the list,
+ * instead you will be given access to the node to be deleted directly.
+ * It is guaranteed that the node to be deleted is not a tail node in the list.
+ * @author zheng
+ * @date 2020/10/20 18:50:58
+ */
+const { ListNode, createList, traversal } = require('./ListNode');
+
+/**
+ * 保证不是尾结点，故待删除结点与后面的结点值不断交换，最后删除最后一个结点
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ * @param {ListNode} node 要删除的结点
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+	if (!node) return;
+	let tail = node;
+	let pre = null;
+	while (tail && tail.next) {
+		pre = tail;
+		tail.val = tail.next.val;
+		tail = tail.next;
+	}
+	pre.next = null;
+};
+
+let arr = [4, 5, 1, 9];
+let head = createList(arr);
+deleteNode(head.next.next); // delete 5
+console.log(traversal(head.next));
