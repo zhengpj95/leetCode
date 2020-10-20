245aa554b868568eb85186fba7dc2d64528a37f2
ZhengAu <598359997@qq.com>
2020-10-20 18:23:23 +0800

    链表判断回文--O(1) memory

--- linkedList/234.palindromeLinkedList.js
+++ linkedList/234.palindromeLinkedList.js
@@ -35,6 +35,51 @@ const isPalindrome = function (head) {
 	return true;
 };
 
-let arr = [1, 2, 3, 3, 4, 2, 1];
+/**
+ * 双指针，链表[a, b)倒序
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome2 = function (head) {
+	if (!head) return true;
+	let fast = head,
+		slow = head;
+
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+	// 奇数个结点，复制插入一个中间结点
+	if (fast) {
+		let newNode = new ListNode(slow.val);
+		newNode.next = slow.next;
+		slow.next = newNode;
+		slow = slow.next;
+	}
+
+	// 前半链表倒序
+	fast = head;
+	let pre = null;
+	while (fast != slow) {
+		let nxt = fast.next;
+		fast.next = pre;
+		pre = fast;
+		fast = nxt;
+	}
+
+	// 两链表判断相等
+	while (slow && pre) {
+		if (slow.val !== pre.val) {
+			return false;
+		}
+		slow = slow.next;
+		pre = pre.next;
+	}
+	return true;
+};
+
+let arr = [1, 2, 3, 4, 5, 3, 2, 1];
 let head = createList(arr);
-console.log(isPalindrome(head.next));
+console.log(isPalindrome2(head.next));
