6cd48f9b70076e02b4fdd843a17775833bae3238
ZhengAu <598359997@qq.com>
2020-10-23 09:58:08 +0800

    质数--优化方法1

--- hashTable/204.countPrimes.js
+++ hashTable/204.countPrimes.js
@@ -6,7 +6,7 @@
 
 /**
  * Approach 1
- * Time complexity: O(n)
+ * Time complexity: O(n^1.5)
  * Space complexity: O(1)
  * @param {number} n
  * @return {number}
@@ -23,12 +23,16 @@ const countPrimes = function (n) {
 };
 
 /**
- * Time complexity: O(n)
+ * Time complexity: O(sqrt(n))
  * @param {number} num
  * @returns {boolean}
  */
 const isPrime = (num) => {
-	for (let i = 2; i <= Math.sqrt(num); i++) {
+	if (num <= 1) return false;
+
+	// Loop's ending condition is i * i <= num instead of i <= sqrt(num)
+	// to avoid repeatedly calling an expensive function sqrt().
+	for (let i = 2; i * i <= num; i++) {
 		if (num % i === 0) return false;
 	}
 	return true;
