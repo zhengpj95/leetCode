86d605efeefd962d8989c4ac8e2abca5af805b4a
ZhengAu <598359997@qq.com>
2020-10-24 22:23:58 +0800

    n个(组合情况

--- backTracking/22.generateParentheses.js
+++ backTracking/22.generateParentheses.js
@@ -1,4 +1,10 @@
 /**
+ * Related Topics: String, BackTracking
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * @author zheng
+ */
+
+/**
  * @param {number} n
  * @return {string[]}
  */
@@ -10,10 +16,10 @@ var generateParenthesis = function (n) {
 
 /**
  * 没有过滤不满足的情况
- * @param {number} level 
- * @param {number} max 
- * @param {string} s 
- * @param {string[]} result 
+ * @param {number} level
+ * @param {number} max
+ * @param {string} s
+ * @param {string[]} result
  */
 var generateNotFilter = function (level, max, s, result) {
 	// 结束条件
@@ -31,11 +37,11 @@ var generateNotFilter = function (level, max, s, result) {
  * 过滤不满足的情况
  * 1. 左括号要小于等于n
  * 2. 右括号也要小于等于n，加入右括号前加入左括号的数量必须多于右括号
- * @param {number} left 
- * @param {number} right 
- * @param {number} n 
- * @param {string} s 
- * @param {string[]} result 
+ * @param {number} left
+ * @param {number} right
+ * @param {number} n
+ * @param {string} s
+ * @param {string[]} result
  */
 var generateWithFilter = function (left, right, n, s, result) {
 	if (left === n && right === n) {
