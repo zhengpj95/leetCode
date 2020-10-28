43020a868f2513d37707590515965dba37bac16b
ZhengAu <598359997@qq.com>
2020-10-28 09:57:16 +0800

    最长无重复子串--优化

--- slindingWindow/3.longestSubstringWithoutRepeatingCharacters.js
+++ slindingWindow/3.longestSubstringWithoutRepeatingCharacters.js
@@ -43,27 +43,61 @@ const lengthOfLongestSubstring = function (s) {
  * @returns {number}
  */
 const lengthOfLongestSubstring2 = function (s) {
-	let map = new Set();
+	let set = new Set();
 	let len = s.length;
-	let star = 0;
-	let end = 0;
+	let left = 0;
+	let right = 0;
 	let max = 0;
 
-	while (star < len && end < len) {
-		if (!map.has(s.charAt(end))) {
-			map.add(s.charAt(end++));
-			max = Math.max(max, end - star);
+	while (left < len && right < len) {
+		if (!set.has(s.charAt(right))) {
+			set.add(s.charAt(right++));
+			max = Math.max(max, right - left);
 		} else {
-			map.delete(s.charAt(star++));
+			set.delete(s.charAt(left++));
 		}
 	}
 	return max;
 };
 
-console.log(lengthOfLongestSubstring2('abcabcbb'));
-console.log(lengthOfLongestSubstring2('bbbbb'));
-console.log(lengthOfLongestSubstring2('pwwkew'));
-console.log(lengthOfLongestSubstring2(' '));
-console.log(lengthOfLongestSubstring2(''));
-console.log(lengthOfLongestSubstring2('au'));
-console.log(lengthOfLongestSubstring2('dvdf'));
+/**
+ * [left, right)
+ * Time complexity : O(2n) = O(n)
+ * Space complexity : O(min(m, n))
+ * @param {string} s
+ * @returns {number}
+ */
+const lengthOfLongestSubstring3 = function (s) {
+	let size = s.length;
+	if (size <= 1) return size;
+
+	let left = 0,
+		right = 0;
+	let window = new Map();
+	let max = 0;
+
+	while (right < size) {
+		let char = s[right];
+		window.set(char, window.has(char) ? window.get(char) + 1 : 1);
+		right++;
+
+		// 保证无重复
+		while (window.get(char) > 1) {
+			let lChar = s[left];
+			window.set(lChar, window.get(lChar) - 1);
+			left++;
+		}
+
+		// [left, right) 左闭右开，此时 right 已经加多 1 了，不需包含 right 处字符
+		max = Math.max(max, right - left);
+	}
+	return max;
+};
+
+console.log(lengthOfLongestSubstring3('abcabcbb'));
+console.log(lengthOfLongestSubstring3('bbbbb'));
+console.log(lengthOfLongestSubstring3('pwwkew'));
+console.log(lengthOfLongestSubstring3(' '));
+console.log(lengthOfLongestSubstring3(''));
+console.log(lengthOfLongestSubstring3('au'));
+console.log(lengthOfLongestSubstring3('dvdf'));
