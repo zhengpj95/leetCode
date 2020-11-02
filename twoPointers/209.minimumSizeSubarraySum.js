dfaedc5f52f47861fd5e7341c7a903bcd14d8b7b
ZhengAu <598359997@qq.com>
2020-11-02 11:49:17 +0800

    子数组>=给定值的子数组长度--binarySearch

--- twoPointers/209.minimumSizeSubarraySum.js
+++ twoPointers/209.minimumSizeSubarraySum.js
@@ -35,8 +35,43 @@ const minSubArrayLen = function (s, nums) {
 	return min === Number.MAX_SAFE_INTEGER ? 0 : min;
 };
 
+/**
+ * Binary Search
+ * Time complexity: O(nlogn) n=s.length
+ * Space complexity: O(n)
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minSubArrayLen2 = function (s, nums) {
+	if (!nums || !nums.length) return 0;
+	let min = Number.MAX_SAFE_INTEGER;
+	let sums = [0];
+	for (let i = 1; i <= nums.length; i++) {
+		sums[i] = sums[i - 1] + nums[i - 1];
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+		// 此处传递的 target 是 sums[i] + s，因为 sums 都是前 i-1 项和
+		// 找到的right与i之间的距离就是大于等于s的子数组长度
+		let right = binarySearch(i + 1, nums.length, sums[i] + s, sums);
+		if (right === sums.length) break;
+		if (min > right - i) min = right - i;
+	}
+	return min === Number.MAX_SAFE_INTEGER ? 0 : min;
+};
+
+const binarySearch = (left, right, target, nums) => {
+	while (left <= right) {
+		let mid = Math.floor((left + right) / 2);
+		if (nums[mid] >= target) right = mid - 1;
+		else left = mid + 1;
+	}
+	return left;
+};
+
 let s = 11,
 	nums = [1, 2, 3, 4, 5];
 (s = 7), (nums = [2, 3, 1, 2, 4, 3]);
-(s = 3), (nums = [1, 1]);
-console.log(minSubArrayLen(s, nums));
+// (s = 3), (nums = [1, 1]);
+console.log(minSubArrayLen2(s, nums));
