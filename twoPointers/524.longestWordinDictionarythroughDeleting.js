468cec9d3e0bae468ed59152efc8a2849bfe91d6
ZhengAu <598359997@qq.com>
2020-11-04 10:36:15 +0800

    通过删除字母匹配到字典里最长单词--直接比较字符串

--- twoPointers/524.longestWordinDictionarythroughDeleting.js
+++ twoPointers/524.longestWordinDictionarythroughDeleting.js
@@ -58,9 +58,37 @@ const findLongestWord = function (s, d) {
 	return result;
 };
 
+/**
+ * 对于数组的每个元素都和 s 比较
+ * Time complexity: O(n*x) Here n refers to the number of strings in list dd and x refers to average string length.
+ * Space complexity : O(x)
+ * @param {string} s
+ * @param {string[]} d
+ * @return {string}
+ */
+const findLongestWord2 = function (s, d) {
+	let result = '';
+	for (let item of d) {
+		if (isSubsequence(s, item)) {
+			if (item.length > result.length || (item.length === result.length && item < result)) result = item;
+		}
+	}
+	return result;
+};
+
+const isSubsequence = (source, pattern) => {
+	let idx = 0;
+	for (let i = 0; i < source.length && idx < pattern.length; i++) {
+		if (source[i] === pattern[idx]) {
+			idx++;
+		}
+	}
+	return idx === pattern.length;
+};
+
 let s = 'abpcplea',
 	d = ['ale', 'apple', 'monkey', 'plea'];
-(s = 'abpcplea'), (d = ['a', 'b', 'c']);
-(s = 'bab'), (d = ['ba', 'ab', 'a', 'b']);
-(s = 'aewfafwafjlwajflwajflwafj'), (d = ['apple', 'ewaf', 'awefawfwaf', 'awef', 'awefe', 'ewafeffewafewf']);
-console.log(`RESULT = `, findLongestWord(s, d));
+// (s = 'abpcplea'), (d = ['a', 'b', 'c']);
+// (s = 'bab'), (d = ['ba', 'ab', 'a', 'b']);
+// (s = 'aewfafwafjlwajflwajflwafj'), (d = ['apple', 'ewaf', 'awefawfwaf', 'awef', 'awefe', 'ewafeffewafewf']);
+console.log(`RESULT = `, findLongestWord2(s, d));
