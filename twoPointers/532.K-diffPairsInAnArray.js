24aff88172a5a296ca921093d48ef71439a6ca83
ZhengAu <598359997@qq.com>
2020-11-04 11:56:51 +0800

    数组中的差值绝对值为k的不同子数组对--map

--- twoPointers/532.K-diffPairsInAnArray.js
+++ twoPointers/532.K-diffPairsInAnArray.js
@@ -66,6 +66,45 @@ const removeDuplicate = (nums) => {
 	return nums.slice(0, idx + 1);
 };
 
+/**
+ * 使用 Map
+ * 如果 k===0 时，从map中检查 nums[i]对应的值>1 即可
+ * 如果 k > 0 时，从map中检查存在 nums[i]+k 即可
+ *
+ * 因为此题是要判断 数组的任两元素之差等于给定值，那么我就可以判断 每个元素值+给定值 是否存在于数组中即可，使用map来判断也可以
+ * Time complexity: O(n) n = nums.length
+ * Space complexity: O(n)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findPairs2 = function (nums, k) {
+	let map = new Map();
+	for (let num of nums) {
+		map.set(num, map.has(num) ? map.get(num) + 1 : 1);
+	}
+
+	let result = 0;
+
+	// // 这里是遍历数组，则需要去重，否则存在重复的情况
+	// nums = removeDuplicate(nums.sort((a, b) => a - b));
+	// for (let num of nums) {
+	// 	if ((k === 0 && map.get(num) > 1) || (k > 0 && map.has(num + k))) {
+	// 		result++;
+	// 	}
+	// }
+
+	// 这里遍历Map.entries，不存在重复的情况
+	result = 0;
+	let entry = map.entries();
+	for (let item of entry) {
+		if ((k === 0 && item[1] > 1) || (k > 0 && map.has(item[0] + k))) {
+			result++;
+		}
+	}
+	return result;
+};
+
 let nums = [3, 1, 4, 1, 5],
 	k = 2;
 // (nums = [1, 2, 3, 4, 5]), (k = 1);
@@ -73,4 +112,5 @@ let nums = [3, 1, 4, 1, 5],
 // (nums = [1, 2, 4, 4, 3, 3, 0, 9, 2, 3]), (k = 3);
 // (nums = [-1, -2, -3]), (k = 1);
 // (nums = [1, 1, 1, 1, 1]), (k = 0);
-console.log(findPairs(nums, k));
+// console.log(findPairs(nums, k));
+console.log(findPairs2(nums, k));
