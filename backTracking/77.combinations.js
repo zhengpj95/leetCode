e74193dfc92a2fbf778d8bef033143fd38ead32f
zhengpj95 <598359997@qq.com>
2020-11-11 10:46:25 +0800

    组合--每k个元素一个组合

---
+++ backTracking/77.combinations.js
@@ -0,0 +1,42 @@
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * You may return the answer in any order.
+ *
+ * Constraints:
+ * 1 <= n <= 20
+ * 1 <= k <= n
+ * @author zheng
+ * @date 2020/11/11 10:40:48
+ */
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+const combine = function (n, k) {
+	let nums = [];
+	for (let i = 1; i <= n; i++) {
+		nums.push(i);
+	}
+
+	let result = [];
+	let backtrack = (nums, track, index) => {
+		if (track.length === k) {
+			result.push([...track]);
+			return;
+		}
+
+		for (let i = index; i < nums.length; i++) {
+			track.push(nums[i]);
+			backtrack(nums, track, i + 1);
+			track.pop();
+		}
+	};
+	backtrack(nums, [], 0);
+	return result;
+};
+
+let n = 4,
+	k = 2;
+console.log(combine(n, k));
