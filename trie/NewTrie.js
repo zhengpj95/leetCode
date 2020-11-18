f6215e724a8069bd1ab35fbd09b83bba6f7b326a
zhengpj95 <598359997@qq.com>
2020-11-18 10:48:06 +0800

    add baseTrieDS

---
+++ trie/NewTrie.js
@@ -0,0 +1,73 @@
+class TrieNode {
+	constructor(val = null) {
+		this.val = val;
+		this.children = [];
+		this.isEnd = false;
+	}
+}
+
+class Trie {
+	constructor() {
+		this.root = new TrieNode();
+	}
+
+	insert(word) {
+		let root = this.root;
+		for (let char of word.split('')) {
+			if (!root.children[char]) {
+				root.children[char] = new TrieNode(char);
+			}
+			root = root.children[char];
+		}
+		root.isEnd = true;
+	}
+
+	search(word) {
+		// let root = this.root;
+		// for (let char of word.split('')) {
+		// 	if (!root.children[char]) {
+		// 		return false;
+		// 	}
+		// 	root = root.children[char];
+		// }
+
+		let root = this.find(word);
+		return root && root.isEnd ? true : false;
+	}
+
+	startWith(prefix) {
+		// let root = this.root;
+		// for (let char of prefix.split('')) {
+		// 	if (!root.children[char]) {
+		// 		return false;
+		// 	}
+		// 	root = root.children[char];
+		// }
+
+		let root = this.find(prefix);
+		return !!root;
+	}
+
+	find(word) {
+		let root = this.root;
+		for (let char of word.split('')) {
+			if (!root.children[char]) {
+				return null;
+			}
+			root = root.children[char];
+		}
+		return root;
+	}
+}
+
+module.exports.Trie = Trie;
+
+// =======test=======
+// let trie = new Trie();
+// trie.insert('bad');
+// trie.insert('javascript');
+// trie.insert('js');
+// console.log(trie.search('3ad'));
+// console.log(trie.startWith('bad'));
+// console.log(trie);
+// console.log(trie.find('j'));
