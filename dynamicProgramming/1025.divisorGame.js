3f9f5d68f354a9b49150a51489e928aace307b56
zhengpj95 <598359997@qq.com>
2020-11-30 09:46:37 +0800

    add 1025 -- add dp solution

--- dynamicProgramming/1025.divisorGame.js
+++ dynamicProgramming/1025.divisorGame.js
@@ -17,3 +17,29 @@
 var divisorGame = function (N) {
 	return N % 2 == 0;
 };
+
+/**
+ * LeetCode他人解法：https://leetcode.com/problems/divisor-game/discuss/323884/c%2B%2B-dp-100-time-complexity
+ *
+ * Let's consider dynomic approach to solve this problem.
+ * Let dp[i] be true if Alice wins and false for the opposite case.
+ * Each i such that 1 <= i <= N is either winning or losing position for Alice.
+ * It's easy to see that if 0 < x < i such that i % x == 0
+ * and if position i - x is winning position for Alice i.e. dp[i - x] == true,
+ * then she can just subtract x from i and move to winning position,
+ * therefore for her i is also winning position.
+ * Now it's only left to consider all possible cases where Alice can win and return dp[N].
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame2 = function (N) {
+	let dp = new Array(N + 1).fill(false);
+	for (let i = 2; i <= N; i++) {
+		for (let j = 1; j * j <= i; j++) {
+			if (i % j === 0 && !dp[i - j]) {
+				dp[i] = true;
+			}
+		}
+	}
+	return dp[N];
+};
