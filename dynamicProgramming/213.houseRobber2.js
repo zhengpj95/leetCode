e3c3930535fd6f5db3dff037b5f8a5c35c349853
zhengpj95 <598359997@qq.com>
2020-12-07 22:59:44 +0800

    add 213 -- add dp solution

---
+++ dynamicProgramming/213.houseRobber2.js
@@ -0,0 +1,40 @@
+/**
+ * You are a professional robber planning to rob houses along a street.
+ * Each house has a certain amount of money stashed.
+ * All houses at this place are arranged in a circle.
+ * That means the first house is the neighbor of the last one.
+ * Meanwhile, adjacent houses have a security system connected,
+ * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ *
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 1000
+ * @author zheng
+ * @date 2020/12/07 22:55:40
+ */
+
+/**
+ * Runtime: 76 ms, faster than 75.62% of JavaScript online submissions for House Robber II.
+ * Memory Usage: 38.5 MB, less than 54.30% of JavaScript online submissions for House Robber II.
+ * 既然最后一位和第一位是相连的，那么抢 houses[1]-houses[n-1] 或者 houses[2]-houses[n]，那么houses[1]和houses[n]就不会被同时抢到了
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+	if (!nums || !nums.length) return 0;
+	if (nums.length <= 3) return Math.max(...nums);
+
+	let dp = [nums[0], Math.max(nums[0], nums[1])];
+	for (let i = 2; i < nums.length - 1; i++) {
+		dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+	}
+
+	let dp1 = [0, nums[1], Math.max(nums[1], nums[2])];
+	for (let i = 3; i < nums.length; i++) {
+		dp1[i] = Math.max(nums[i] + dp1[i - 2], dp1[i - 1]);
+	}
+	return Math.max(dp[dp.length - 1], dp1[dp1.length - 1]);
+};
