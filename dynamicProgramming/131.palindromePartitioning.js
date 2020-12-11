a607d844f65b2c932c676b453a86570329a71cb7
zhengpj95 <598359997@qq.com>
2020-12-11 22:52:54 +0800

    update 131 -- add backtracking + dp solution

--- dynamicProgramming/131.palindromePartitioning.js
+++ dynamicProgramming/131.palindromePartitioning.js
@@ -10,6 +10,7 @@
  */
 
 /**
+ * Approach 1: Backtracking
  * Time complexity: O(N*2^N)
  *		where N is the length of string s.
  * 		This is the worst-case time complexity when all the possible substrings are palindrome.
@@ -56,6 +57,46 @@ var isPalindrome = (s, low, high) => {
 		if (s[low++] != s[high--]) return false;
 	}
 	return true;
+};
+
+/**
+ * Backtracking + DP
+ * Time Complexity : O(N*2^N), where N is the length of string s. In the worst case, there could be 2^N possible substrings
+ * 		and it will take O(N) to generate each substring using substr as in Approach 1.
+ * 		However, we are eliminating one additional iteration to check if substring is a palindrome or not.
+ * Space Complexity: O(N*N), where N is the length of the string s.
+ * 		The recursive call stack would require N space as in Approach 1.
+ * 		Additionally we also use 2 dimensional array dp of size N*N
+ * This gives us total space complexity as O(N*N) + O(N) = O(N*N)
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partitionWithDP = function (s) {
+	let result = [];
+	let dp = new Array(s.length);
+	for (let i = 0; i < dp.length; i++) {
+		dp[i] = new Array(s.length).fill(0);
+	}
+
+	let backtrack = (track, start) => {
+		if (start >= s.length) {
+			result.push([...track]);
+		}
+
+		for (let end = start; end < s.length; end++) {
+			// end-start <= 2 ==> 'a', 'aa', 'aca' is palindrome
+			// why dp[start+1][end-1]? because backtracking is recursion,
+			// when operate dp[start][end], dp[start+1][end-1] is done.
+			if (s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])) {
+				dp[start][end] = 1;
+				track.push(s.substring(start, end + 1));
+				backtrack(track, end + 1);
+				track.pop();
+			}
+		}
+	};
+	backtrack([], 0);
+	return result;
 };
 
 let s = 'aaa';
