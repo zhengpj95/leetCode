9bb7656324ad9284b60609b545e850f181ae0733
zhengpj95 <598359997@qq.com>
2020-12-12 23:10:12 +0800

    add 258 -- add brute force sulotion

---
+++ math/258.addDigits.js
@@ -0,0 +1,37 @@
+/**
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ * Follow up: Could you do it without any loop/recursion in O(1) runtime?
+ * @author zheng
+ * @date 2020/12/12 23:01:29
+ */
+
+/**
+ * O(T) = O(n^2) n是num的长度
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+	while ((num + '').length > 1) {
+		let str = num + '';
+		let num1 = 0;
+		for (let i = 0; i < str.length; i++) {
+			num1 += parseInt(str[i]);
+		}
+		num = num1;
+	}
+	return num;
+};
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+	let result = 0;
+	while (num > 0) {
+		result += num % 10;
+		num /= 10;
+	}
+	return result;
+};
