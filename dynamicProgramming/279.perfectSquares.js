5f1b9eeafc54598cc01e394798aa73604c4160fe
zhengpj95 <598359997@qq.com>
2020-12-15 23:03:18 +0800

    udpate 279 -- add dp solution

--- dynamicProgramming/279.perfectSquares.js
+++ dynamicProgramming/279.perfectSquares.js
@@ -1,5 +1,5 @@
 /**
- *
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
  * @author zheng
  * @date 2020/12/15 22:48:40
  */
@@ -12,25 +12,20 @@
 var numSquares = function (n) {
 	if (n <= 0) return 0;
 
-	// 凑数的种类
+	// 类似凑银币问题，prime是硬币类型
 	let prime = [0, 1];
-	for (let i = 2; i * i <= n; i++) {
+	for (let i = 2; i <= n; i++) {
 		prime[i] = i * i;
 	}
-
-	if (prime.indexOf(n) >= 0) return 1;
+	if (prime.includes(n)) return 1;
 
 	let dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
 	dp[0] = 0;
 	for (let i = 1; i <= n; i++) {
 		for (let j = 1; j < prime.length; j++) {
-			if (i < prime[j]) {
-				// 当前凑数的种类比要凑的数大了（硬币种类比要凑的币值都大了）
-				break;
-			}
+			if (i < prime[j]) break; //币值大于要凑的数，不符合了
 			dp[i] = Math.min(dp[i], dp[i - prime[j]] + 1);
 		}
 	}
-	// console.log(dp)
 	return dp[n];
 };
