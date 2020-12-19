4951cf6a07f5f33889d7e0298a1161fa8850d886
zhengpj95 <598359997@qq.com>
2020-12-19 23:28:19 +0800

    add 709.tolowercase

---
+++ string/709.toLowerCase.js
@@ -0,0 +1,13 @@
+/**
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ * @author zheng
+ * @date 2020/12/19 23:20:19
+ */
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+	return str.toLowerCase();
+};
