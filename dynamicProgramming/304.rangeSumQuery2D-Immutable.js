1f099d93b22dfccfe93f071c96200751699ab7d6
ZhengAu <598359997@qq.com>
2020-12-20 14:35:34 +0800

    update 304 -- add dp solution

--- dynamicProgramming/304.rangeSumQuery2D-Immutable.js
+++ dynamicProgramming/304.rangeSumQuery2D-Immutable.js
@@ -12,23 +12,71 @@
 
 // =======================Approach 1: Brute Force=======================
 
-var matrix = [];
+// var matrix = [];
+
+// /**
+//  * Runtime: 172 ms, faster than 27.76% of JavaScript online submissions for Range Sum Query 2D - Immutable.
+//  * Memory Usage: 43.4 MB, less than 68.06% of JavaScript online submissions for Range Sum Query 2D - Immutable.
+//  *
+//  * Time complexity : O(mn) time per query.
+//  * 		Assume that m and n represents the number of rows and columns respectively,
+//  * 		each sumRegion query can go through at most times m*n elements.
+//  Space complexity : O(1). Note that data is a reference to matrix and is not a copy of it.
+//  * @param {number[][]} matrix
+//  */
+// var NumMatrix = function (matrix) {
+// 	this.matrix = matrix;
+// };
+
+// /**
+//  * @param {number} row1
+//  * @param {number} col1
+//  * @param {number} row2
+//  * @param {number} col2
+//  * @return {number}
+//  */
+// NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+// 	let sum = 0;
+// 	for (let row = row1; row <= row2; row++) {
+// 		for (let col = col1; col <= col2; col++) {
+// 			sum += this.matrix[row][col];
+// 		}
+// 	}
+// 	return sum;
+// };
+
+// /**
+//  * Your NumMatrix object will be instantiated and called as such:
+//  * var obj = new NumMatrix(matrix)
+//  * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+//  */
+
+// =======================Approach 2: DP=======================
 
 /**
- * Runtime: 172 ms, faster than 27.76% of JavaScript online submissions for Range Sum Query 2D - Immutable.
- * Memory Usage: 43.4 MB, less than 68.06% of JavaScript online submissions for Range Sum Query 2D - Immutable.
- * 
- * Time complexity : O(mn) time per query. 
- * 		Assume that m and n represents the number of rows and columns respectively, 
- * 		each sumRegion query can go through at most times m*n elements.
- Space complexity : O(1). Note that data is a reference to matrix and is not a copy of it.
+ * Time complexity: O(m*n)
+ * Space complexity: O(m*n)
+ * Runtime: 100 ms, faster than 90.49% of JavaScript online submissions for Range Sum Query 2D - Immutable.
+ * Memory Usage: 43.5 MB, less than 47.53% of JavaScript online submissions for Range Sum Query 2D - Immutable.
  * @param {number[][]} matrix
  */
 var NumMatrix = function (matrix) {
-	this.matrix = matrix;
+	this.dp = matrix;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[0].length; j++) {
+			this.dp[i][j] += this.getCell(i - 1, j) + this.getCell(i, j - 1) - this.getCell(i - 1, j - 1);
+		}
+	}
+};
+
+NumMatrix.prototype.dp = [];
+
+NumMatrix.prototype.getCell = function (row, col) {
+	return row >= 0 && col >= 0 ? this.dp[row][col] : 0;
 };
 
 /**
+ * Time complexity: O(1)
  * @param {number} row1
  * @param {number} col1
  * @param {number} row2
@@ -36,13 +84,7 @@ var NumMatrix = function (matrix) {
  * @return {number}
  */
 NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
-	let sum = 0;
-	for (let row = row1; row <= row2; row++) {
-		for (let col = col1; col <= col2; col++) {
-			sum += this.matrix[row][col];
-		}
-	}
-	return sum;
+	return this.getCell(row2, col2) - this.getCell(row2, col1 - 1) - this.getCell(row1 - 1, col2) + this.getCell(row1 - 1, col1 - 1);
 };
 
 /**
