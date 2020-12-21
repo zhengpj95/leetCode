ed2ad045505733d6dcb9b7a74eab108fe7801b31
zhengpj95 <598359997@qq.com>
2020-12-21 21:17:16 +0800

    update 309 -- udpate dp solution

--- dynamicProgramming/309.bestTimetoBuyandSellStockwithCooldown.js
+++ dynamicProgramming/309.bestTimetoBuyandSellStockwithCooldown.js
@@ -33,3 +33,51 @@ var maxProfit = function (prices) {
 
 	return Math.max(sold[size], rest[size]);
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit2 = function (prices) {
+	if (!prices || prices.length <= 1) return 0;
+	let size = prices.length;
+	let hold = new Array(size); //持有
+	let sold = new Array(size); //卖出
+	let rest = new Array(size); //cooldown
+
+	sold[0] = Number.MIN_SAFE_INTEGER;
+	rest[0] = 0; //开始时，未有买卖
+	hold[0] = -prices[0]; //买入后，持有-prices[0]
+
+	for (let i = 1; i < size; i++) {
+		sold[i] = hold[i - 1] + prices[i];
+		rest[i] = Math.max(rest[i - 1], sold[i - 1]);
+		hold[i] = Math.max(hold[i - 1], rest[i - 1] - prices[i]);
+	}
+
+	return Math.max(sold[size - 1], rest[size - 1]);
+};
+
+/**
+ * Runtime: 76 ms, faster than 93.53% of JavaScript online submissions for Best Time to Buy and Sell Stock with Cooldown.
+ * Memory Usage: 38.1 MB, less than 100.00% of JavaScript online submissions for Best Time to Buy and Sell Stock with Cooldown.
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit3 = function (prices) {
+	if (!prices || prices.length <= 1) return 0;
+
+	let hold = -prices[0];
+	let sold = Number.MIN_SAFE_INTEGER;
+	let rest = 0;
+
+	for (let i = 1; i < prices.length; i++) {
+		let preSold = sold,
+			preRest = rest;
+		sold = hold + prices[i];
+		rest = Math.max(rest, preSold);
+		hold = Math.max(hold, preRest - prices[i]);
+	}
+
+	return Math.max(sold, rest);
+};
