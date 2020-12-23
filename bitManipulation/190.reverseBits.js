64754aa9ec8a5b153308676faa7813e47b02c7a7
zhengpj95 <598359997@qq.com>
2020-12-23 23:26:23 +0800

    add 190

---
+++ bitManipulation/190.reverseBits.js
@@ -0,0 +1,21 @@
+/**
+ * Reverse bits of a given 32 bits unsigned integer.
+ *
+ * Constraints: The input must be a binary string of length 32
+ * @author zheng
+ * @date 2020/12/23 23:24:37
+ */
+
+/**
+ * 用 *2 替代 <<1，因为JavaScript中32位的有符号位移位操作会出现负数，特殊的bug
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+	let result = 0;
+	for (let i = 0; i < 32; i++) {
+		result = result * 2 + (n & 1);
+		n = n >> 1;
+	}
+	return result;
+};
