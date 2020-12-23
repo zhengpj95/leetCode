890c44e1f81f4cdd280047cece022ee30f2052a2
zhengpj95 <598359997@qq.com>
2020-12-23 23:16:03 +0800

    add 190 -- add cpp solution

---
+++ 190.ReverseBits.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+class Solution
+{
+public:
+	uint32_t reverseBits(uint32_t n)
+	{
+		uint32_t res = 0;
+		for (int i = 31; i >= 0; i--)
+		{
+			res += ((n & 1) << i);
+			n >>= 1;
+		}
+		return res;
+	}
+};
+
+int main()
+{
+	return 0;
+}
