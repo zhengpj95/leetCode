557aa82350b9217a6e714aeeb3dd58c39f149302
zhengpj95 <598359997@qq.com>
2020-12-24 09:05:50 +0800

    update 190

--- bitManipulation/190.reverseBits.js
+++ bitManipulation/190.reverseBits.js
@@ -19,3 +19,16 @@ var reverseBits = function (n) {
 	}
 	return result;
 };
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits2 = function (n) {
+	let result = 0;
+	for (let i = 0; i < 32; i++) {
+		result = (result << 1) + (n & 1);
+		n = n >> 1;
+	}
+	return result >>> 0;
+};
