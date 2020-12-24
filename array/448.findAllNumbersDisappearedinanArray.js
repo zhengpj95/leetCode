3a5b9635b3ee19bc9b1df18993150a40bccda28b
zhengpj95 <598359997@qq.com>
2020-12-24 23:41:14 +0800

    update 448 -- add O(1) memory sulotion

--- array/448.findAllNumbersDisappearedinanArray.js
+++ array/448.findAllNumbersDisappearedinanArray.js
@@ -27,3 +27,28 @@ var findDisappearedNumbers = function (nums) {
 	}
 	return res;
 };
+
+/**
+ * Runtime: 96 ms, faster than 99.71% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
+ * Memory Usage: 46.4 MB, less than 74.80% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers2 = function (nums) {
+	// 让每一个元素对应的下标的元素变成负数，剩下的正数就是所求了
+	for (let i = 0; i < nums.length; i++) {
+		let idx = Math.abs(nums[i]) - 1;
+		nums[idx] = Math.abs(nums[idx]) * -1;
+	}
+
+	let res = [];
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] > 0) {
+			res.push(i + 1);
+		}
+	}
+	return res;
+};
+
+let nums = [4, 3, 2, 7, 8, 2, 3, 1];
+console.log(findDisappearedNumbers2(nums));
