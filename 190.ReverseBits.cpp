a13b5b8102fd4ef33047e4db2ff66ea2cdfa1f61
zhengpj95 <598359997@qq.com>
2020-12-24 22:44:38 +0800

    update 190

--- 190.ReverseBits.cpp
+++ 190.ReverseBits.cpp
@@ -7,17 +7,9 @@ public:
 	uint32_t reverseBits(uint32_t n)
 	{
 		uint32_t res = 0;
-		// for (int i = 31; i >= 0; i--)
-		// {
-		// 	res += ((n & 1) << i);
-		// 	n >>= 1;
-		// }
-
-		for (int i = 0; i < 32; i++)
+		for (int i = 31; i >= 0; i--)
 		{
-			res <<= 1;
-			res += (n & 1);
-			// res = (res << 1) + (n & 1);
+			res += ((n & 1) << i);
 			n >>= 1;
 		}
 		return res;
