626463785c414185d4e8922b8bc69d35ca91f4ca
zhengpj95 <598359997@qq.com>
2020-12-30 22:32:12 +0800

    update 342 -- bitmanipulation

--- bitManipulation/342.powerofFour.js
+++ bitManipulation/342.powerofFour.js
@@ -30,3 +30,17 @@ var isPowerOfFour = function (n) {
 var isPowerOfFour2 = function (n) {
 	return /^1(00)*$/.test(n.toString(2));
 };
+
+/**
+ * 4的二进制就是0b0100，n是4的倍数，则其二进制中就只能有一个1
+ * 所以 n & (n - 1) == 0 能判断一个数是否是偶数
+ * 0xaaaaaaaa & n 能判断其是否是4的倍数。0xaaaaaaaa 对应的二进制就是 0b1010101010101010，其0所处的位置刚刚好是4的倍数位
+ *
+ * Runtime: 84 ms, faster than 97.40% of JavaScript online submissions for Power of Four.
+ * Memory Usage: 39.9 MB, less than 71.10% of JavaScript online submissions for Power of Four.
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour3 = function (n) {
+	return n > 0 && (n & (n - 1)) == 0 && (0xaaaaaaaa & n) == 0;
+};
