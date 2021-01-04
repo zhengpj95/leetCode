aa5b9e603a775726edaf2538342923e4005cc31c
zhengpj95 <598359997@qq.com>
2021-01-04 22:14:39 +0800

    add 383

---
+++ string/383.ransomNote.js
@@ -0,0 +1,30 @@
+/**
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines,
+ * write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+ * Each letter in the magazine string can only be used once in your ransom note.
+ *
+ * Constraints: You may assume that both strings contain only lowercase letters.
+ * @author zheng
+ * @date 2021/01/04 22:14:06
+ */
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+	let map = new Map();
+	for (let i = 0; i < magazine.length; i++) {
+		map.set(magazine[i], map.has(magazine[i]) ? map.get(magazine[i]) + 1 : 1);
+	}
+
+	for (let i = 0; i < ransomNote.length; i++) {
+		let char = ransomNote[i];
+		if (!map.has(char) || map.get(char) <= 0) {
+			return false;
+		}
+		map.set(char, map.get(char) - 1);
+	}
+	return true;
+};
