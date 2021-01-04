a655e563f20460f2e5701e2ee1e98e84b9b164ae
zhengpj95 <598359997@qq.com>
2021-01-04 22:16:52 +0800

    update 383

--- string/383.ransomNote.js
+++ string/383.ransomNote.js
@@ -28,3 +28,20 @@ var canConstruct = function (ransomNote, magazine) {
 	}
 	return true;
 };
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct2 = function (ransomNote, magazine) {
+	for (let i = 0; i < ransomNote.length; i++) {
+		let char = ransomNote[i];
+		if (magazine.includes(char)) {
+			magazine = magazine.replace(char, '');
+		} else {
+			return false;
+		}
+	}
+	return true;
+};
