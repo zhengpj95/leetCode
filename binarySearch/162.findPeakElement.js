471e70bac6c31124c33e56306a4476dff2a9458f
zhengpj95 <598359997@qq.com>
2021-01-11 22:37:46 +0800

    add 162

---
+++ binarySearch/162.findPeakElement.js
@@ -0,0 +1,24 @@
+/**
+ * A peak element is an element that is strictly greater than its neighbors.
+ * Given an integer array nums, find a peak element, and return its index.
+ * If the array contains multiple peaks, return the index to any of the peaks
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ *
+ * Follow up: Could you implement a solution with logarithmic complexity?
+ * @author zheng
+ * @date 2021/01/11 22:37:14
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+	let idx = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[idx] < nums[i]) {
+			idx = i;
+		}
+	}
+	return idx;
+};
