0e12fb217b65566ee14a2c1fd7973e5b5fd0baba
zhengpj95 <598359997@qq.com>
2021-01-21 17:48:51 +0800

    add sort/912.sortanArray

---
+++ sort/912.sortanArray.js
@@ -0,0 +1,13 @@
+/**
+ * Given an array of integers nums, sort the array in ascending order.
+ * @author zheng
+ * @date 2021/01/21 17:48:01
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+	return nums.sort((a, b) => a - b);
+};
