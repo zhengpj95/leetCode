b2c485a74dd4f9d58a971713e4f40c9b7b5c9a43
zhengpj95 <598359997@qq.com>
2021-01-22 17:28:12 +0800

    add 877

---
+++ dynamicProgramming/877.stoneGame.js
@@ -0,0 +1,13 @@
+/**
+ * @author zheng
+ * @date 2021/01/22 17:26:38
+ */
+
+/**
+ * 不需要按石子堆的顺序取，玩家总是取最多的那一堆就可以了
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+	return true;
+};
