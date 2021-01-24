f07c83025a055da32ddc11bc76eaf31a2d66fe67
zhengpj95 <598359997@qq.com>
2021-01-24 21:23:39 +0800

    update 647.palidromicSubstrings

--- dynamicProgramming/647.palindromicSubstrings.js
+++ dynamicProgramming/647.palindromicSubstrings.js
@@ -30,3 +30,63 @@ var countSubstrings = function (s) {
 	}
 	return count;
 };
+
+/**
+ * Approach 2: Recursion + Map
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings2 = function (s) {
+	let map = new Map();
+	let isPalidrome = (s, low, high) => {
+		let str = s.slice(low, high + 1);
+		if (map.get(str)) {
+			return true;
+		}
+		while (low < high) {
+			if (s[low++] != s[high--]) {
+				map.set(str, false);
+				return false;
+			}
+		}
+		map.set(str, true);
+		return true;
+	};
+	let count = 0;
+	for (let i = 0; i < s.length; i++) {
+		for (let j = i; j < s.length; j++) {
+			count += isPalidrome(s, i, j);
+		}
+	}
+	return count;
+};
+
+/**
+ * Approach 3: DP
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings3 = function (s) {
+	if (s.length <= 0) return 0;
+	let size = s.length;
+	let count = size;
+	let dp = [];
+	// single letter substrings
+	for (let i = 0; i < size; i++) {
+		dp[i] = [];
+		dp[i][i] = true;
+	}
+	// double letter substrings
+	for (let i = 0; i < size - 1; i++) {
+		dp[i][i + 1] = s[i] == s[i + 1];
+		count += dp[i][i + 1] ? 1 : 0;
+	}
+	// substrings of length 3 to n
+	for (let len = 3; len <= size; ++len) {
+		for (let i = 0, j = i + len - 1; j < size; ++i, ++j) {
+			dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j];
+			count += dp[i][j] ? 1 : 0;
+		}
+	}
+	return count;
+};
