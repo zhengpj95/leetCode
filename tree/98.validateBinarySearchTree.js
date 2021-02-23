5ca0ca8852fd4663e6133aa7eb58c4618a9fba1e
zhengpj95 <598359997@qq.com>
2021-02-23 16:11:00 +0800

    update

--- tree/98.validateBinarySearchTree.js
+++ tree/98.validateBinarySearchTree.js
@@ -1,19 +1,13 @@
-
-//  Definition for a binary tree node.
-function TreeNode(val, left, right) {
-	this.val = (val === undefined ? 0 : val);
-	this.left = (left === undefined ? null : left);
-	this.right = (right === undefined ? null : right);
-}
+const { TreeNode, createTree } = require('./TreeNode');
 
 /**
  * Given a binary tree, determine if it is a valid binary search tree (BST).
- * 
+ *
  * Assume a BST is defined as follows:
  * 1. The left subtree of a node contains only nodes with keys less than the node's key.
  * 2. The right subtree of a node contains only nodes with keys greater than the node's key.
  * 3. Both the left and right subtrees must also be binary search trees.
- * 
+ *
  * Time complexity: O(n) in the worst case when the tree is BST or the "bad" element is a rightmost leaf.
  * Space complexity: O(n) to keep stack.
  * @param {TreeNode} root
@@ -22,7 +16,7 @@ function TreeNode(val, left, right) {
 var isValidBST = function (root) {
 	// inorder traversal
 	let stack = [];
-	let preNode = null;
+	let preNode = null; //中序遍历过程中的前驱结点
 	while (root || stack.length) {
 		while (root) {
 			stack.push(root);
@@ -41,8 +35,7 @@ var isValidBST = function (root) {
 	return true;
 };
 
-
-/** 
+/**
  * @param {TreeNode} root
  * @return {boolean}
  */
@@ -72,23 +65,6 @@ var isValidBST2 = function (root) {
 	return true;
 };
 
-let node0 = new TreeNode(0);
-let node1 = new TreeNode(1);
-let node2 = new TreeNode(2);
-let node3 = new TreeNode(3);
-let node4 = new TreeNode(4);
-let node5 = new TreeNode(5);
-let node6 = new TreeNode(6);
-
-node3.left = node1;
-node3.right = node5;
-node1.left = node0;
-node1.right = node2;
-node5.left = node4;
-node5.right = node6;
-
-// node2.left = node1;
-// node2.right = node3;
-
-let res = isValidBST(node2);
+let root = createTree([2, 1, 3]); //createTree([5, 1, 4, null, null, 3, 6]);
+let res = isValidBST(root);
 console.log(res);
