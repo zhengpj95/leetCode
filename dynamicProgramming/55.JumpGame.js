f8791abef880b5db4ad49e1d0e5119b708dd82b3
zhengpj <598359997@qq.com>
2021-09-27 11:02:35 +0800

    update 55, solve with greedy

--- dynamicProgramming/55.JumpGame.js
+++ dynamicProgramming/55.JumpGame.js
@@ -27,5 +27,35 @@ var canJumpWithDP = function (nums) {
 	return dp[dp.length - 1] != Number.MAX_SAFE_INTEGER;
 };
 
-console.log(canJumpWithDP([3, 2, 1, 0, 4]));
-console.log(canJumpWithDP([2, 3, 1, 1, 4]));
+/**
+ * O(T)=O(n)
+ * O(S)=O(n)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJumpWithGreedy = function (nums) {
+	if (!nums || !nums.length) {
+		return false;
+	}
+
+	let curEnd = 0;
+	let maxStep = 0;
+	for (let i = 0; i < nums.length - 1; i++) {
+		maxStep = Math.max(maxStep, nums[i] + i);
+		// console.log(`${i} - ${nums[i] + i} - ${maxStep}`);
+
+		// 当前元素是0的，且其不等于最后一个，而且最大步数还是当前，已进入死循环
+		if (maxStep == nums[i] + i && nums[i] == 0 && i != nums.length - 1) {
+			return false;
+		}
+		if (i == curEnd) {
+			curEnd = maxStep;
+		}
+	}
+	return maxStep >= nums.length - 1;
+};
+
+console.log(canJumpWithGreedy([2, 3, 1, 1, 4]));
+console.log(canJumpWithGreedy([3, 2, 1, 0, 4]));
+console.log(canJumpWithGreedy([0, 2, 3]));
+console.log(canJumpWithGreedy([1, 0, 1, 0]));
