36014f82c6df1724bac346ec69cde768399075cc
zhengpj <598359997@qq.com>
2021-11-01 20:46:17 +0800

    update 130

--- search/130.SurroundedRegions.js
+++ search/130.SurroundedRegions.js
@@ -1,6 +1,12 @@
 /**
  * 从4个角落开始，把边上为 O 的及其相连接的都设为另一个值 Z
  * 最后遍历整个board，把 Z 变成 O，其余的都是 X 了
+ *
+ * 从正向思考时候，你需要找出 O 周边的情况，如果是单独一个O还好处理，若是多个O相连，就需要找出这块O区域的周边情况
+ * 那么就要考虑从反面思考了，找出O以及与其相连，都让它置为另一个值Z，就可以从四个角落往中间收缩，已经遍历过的若是原来为O的必变成了其他值Z
+ *
+ * Time Complexity: O(rows*cols)
+ * Space Complexity: O(rows*cols) dfs处理过程中的堆栈
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
