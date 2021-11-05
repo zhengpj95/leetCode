4b3c15e67cb7c95c1b4357640f0c94a8b9ffcd4b
zhengpj <598359997@qq.com>
2021-11-05 16:18:15 +0800

    update 441

--- binarySearch/441.ArrangingCoins.js
+++ binarySearch/441.ArrangingCoins.js
@@ -5,3 +5,25 @@
 var arrangeCoins = function (n) {
 	return Math.floor(Math.sqrt(2 * n + 1 / 4) - 1 / 2);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoinsWithBinarySearch = function (n) {
+	let low = 0,
+		high = n;
+	while (low <= high) {
+		let row = Math.floor((low + high) / 2); //当前层
+		let coins = Math.floor((row * (row + 1)) / 2); //1到row层所拥有的coins数量
+		if (coins == n) {
+			return row;
+		}
+		if (n < coins) {
+			high = row - 1;
+		} else {
+			low = row + 1;
+		}
+	}
+	return high;
+};
