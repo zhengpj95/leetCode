e24577dc32e67461ee750c0244b0b3f7fbd1acf3
zhengpj <598359997@qq.com>
2021-12-04 13:12:06 +0800

    add 1032

---
+++ trie/1032.StreamofCharacters.js
@@ -0,0 +1,46 @@
+/**
+ * 因为查询是suffix，所以构造trie时，倒序插入构造
+ * @param {string[]} words
+ */
+var StreamChecker = function (words) {
+	this.trie = {};
+	this.letters = [];
+	for (let word of words) {
+		word = word.split('').reverse().join('');
+		let node = this.trie;
+		for (let c of word) {
+			if (!node[c]) node[c] = {};
+			node = node[c];
+		}
+		node['isWord'] = true;
+	}
+};
+
+/**
+ * @param {character} letter
+ * @return {boolean}
+ */
+StreamChecker.prototype.query = function (letter) {
+	this.letters.push(letter);
+	let node = this.trie;
+	for (let i = this.letters.length - 1; i >= 0; i--) {
+		if (node[this.letters[i]]) {
+			node = node[this.letters[i]];
+			if (node['isWord']) {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+	return false;
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * var obj = new StreamChecker(words)
+ * var param_1 = obj.query(letter)
+ */
+
+let o = new StreamChecker(['cd', 'f', 'kl']);
+console.log(o);
