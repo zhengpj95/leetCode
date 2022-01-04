f3d56b5d0a53be94bbf8d1535be83c740fc0b104
zhengpj <598359997@qq.com>
2022-01-04 10:54:07 +0800

    update 476

--- bitManipulation/476.NumberComplement.js
+++ bitManipulation/476.NumberComplement.js
@@ -6,6 +6,9 @@
     // let newNum = num.toString(2).split('').map(x => x == '1' ? '0' : '1').join('');
     // return parseInt(newNum, 2);
 
+    if (!num) {
+        return 1;
+    }
     let size = Math.floor(Math.log2(num) + 1);
     let mask = 2 ** size - 1;
     return num ^ mask;
