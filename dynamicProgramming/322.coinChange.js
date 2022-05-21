aad03abc304ee275201d1ded4e51821d29281714
zhengpj <598359997@qq.com>
2022-05-21 11:14:50 +0800

    update 322

--- dynamicProgramming/322.coinChange.js
+++ dynamicProgramming/322.coinChange.js
@@ -51,3 +51,20 @@ var coinChange2 = function (coins, amount) {
 	}
 	return dp[dp.length - 1] == amount + 1 ? -1 : dp[dp.length - 1];
 };
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange3 = function (coins, amount) {
+	let dp = Array(amount + 1).fill(Infinity);
+	dp[0] = 0;
+
+	for (let i = 0; i < coins.length; i++) {
+		for (let j = coins[i]; j <= amount; j++) {
+			dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+		}
+	}
+	return dp[amount] == Infinity ? -1 : dp[amount];
+};
