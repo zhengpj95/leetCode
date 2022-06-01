4ef33d7ef5a8626c472f4d1f579f69eccff0329a
zhengpj <598359997@qq.com>
2022-06-01 10:01:30 +0800

    add 1480

---
+++ array/1480.RunningSumof_1d_Array.js
@@ -0,0 +1,13 @@
+/**
+ * Prefix Sum
+ * O(T): O(n)
+ * O(S): O(1)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+	for (let i = 1; i < nums.length; i++) {
+		nums[i] = nums[i - 1] + nums[i];
+	}
+	return nums;
+};
