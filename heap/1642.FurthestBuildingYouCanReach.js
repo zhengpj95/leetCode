4eed08c384fd451db1149af3a46c989b07ee3d8e
zhengpj <598359997@qq.com>
2022-06-21 12:26:29 +0800

    add PriorityQueue and 1642

---
+++ heap/1642.FurthestBuildingYouCanReach.js
@@ -0,0 +1,50 @@
+/**
+ * You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+ * You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+ * While moving from building i to building i+1 (0-indexed),
+ * If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+ * If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+ * Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+ */
+
+const { PriorityQueue } = require("./PriorityQueue");
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+var furthestBuilding = function (heights, bricks, ladders) {
+	let queue = new PriorityQueue((a, b) => a < b);
+	let count = 0;
+	for (let i = 0; i < heights.length - 1; i++) {
+		// next building is maller than current, jump it
+		if (heights[i] >= heights[i + 1]) {
+			continue;
+		}
+		let diff = heights[i + 1] - heights[i];
+		count += diff;
+		// count of needed bricks is greater than given bricks
+		if (ladders == 0 && count > bricks) {
+			return i;
+		}
+		// continue move to next building
+		if (ladders == 0 && count <= bricks) {
+			continue;
+		}
+		queue.enqueue(diff);
+		// get the max diff, use ladders to replace it needed bricks
+		if (count > bricks) {
+			let top = queue.dequeue();
+			count -= top;
+			ladders--;
+		}
+	}
+	return heights.length - 1;
+};
+let heights = [4, 2, 7, 6, 9, 14, 12],
+	bricks = 5,
+	ladders = 1;
+// (heights = [4, 12, 2, 7, 3, 18, 20, 3, 19]), (bricks = 10), (ladders = 2);
+// (heights = [14, 3, 19, 3]), (bricks = 17), (ladders = 0);
+console.log(furthestBuilding(heights, bricks, ladders));
---
+++ heap/PriorityQueue.js
@@ -0,0 +1,83 @@
+/**
+ * 优先队列
+ */
+class PriorityQueue {
+	// compare函数，比较大小，使用 > < >= <=
+	compare(a, b) {
+		return a > b;
+	}
+	/**
+	 * 传入比较排序函数，默认从小到大排序输出
+	 * @param {Function} compare
+	 */
+	constructor(compare = this.compare) {
+		this.count = 0;
+		this.list = [];
+		this.compare = compare;
+	}
+
+	isEmpty() {
+		return this.count == 0;
+	}
+
+	getTop() {
+		return this.isEmpty() ? null : this.list[1];
+	}
+
+	getParent(k = 1) {
+		return Math.floor(k / 2);
+	}
+
+	leftChildren(k = 1) {
+		return k * 2;
+	}
+
+	rightChildren(k = 1) {
+		return k * 2 + 1;
+	}
+
+	exchange(i, j) {
+		if (i > this.count || j > this.count) return;
+		let temp = this.list[i];
+		this.list[i] = this.list[j];
+		this.list[j] = temp;
+	}
+
+	isLarge(i, j) {
+		return this.compare(this.list[i], this.list[j]);
+	}
+
+	swim(k = this.count) {
+		while (k > 1 && this.isLarge(this.getParent(k), k)) {
+			this.exchange(this.getParent(k), k);
+			k = this.getParent(k);
+		}
+	}
+
+	sink(k = 1) {
+		while (this.leftChildren(k) <= this.count) {
+			let max = this.leftChildren(k);
+			if (this.rightChildren(k) <= this.count && this.isLarge(max, this.rightChildren(k))) {
+				max = this.rightChildren(k);
+			}
+			if (this.isLarge(max, k)) break;
+			this.exchange(k, max);
+			k = max;
+		}
+	}
+
+	enqueue(ele) {
+		this.list[++this.count] = ele;
+		this.swim();
+	}
+
+	dequeue() {
+		if (this.isEmpty()) return null;
+		let max = this.list[1];
+		this.exchange(1, this.count);
+		this.list[this.count--] = null;
+		this.sink();
+		return max;
+	}
+}
+module.exports.PriorityQueue = PriorityQueue;
