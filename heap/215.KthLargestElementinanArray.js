3e84b9142f8389226af1ff972a3e65d3eafc3f45
zhengpj <598359997@qq.com>
2022-06-23 11:44:48 +0800

    update 215

--- heap/215.KthLargestElementinanArray.js
+++ heap/215.KthLargestElementinanArray.js
@@ -6,11 +6,25 @@
  * @date 2021/01/06 23:14:42
  */
 
+const { PriorityQueue } = require("./PriorityQueue");
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-	return nums.sort((a, b) => b - a)[k - 1];
+	// return nums.sort((a, b) => b - a)[k - 1];
+
+	let queue = new PriorityQueue((a, b) => a < b);
+	nums.forEach((item) => queue.enqueue(item));
+	while (k-- > 1) {
+		queue.dequeue();
+	}
+	return queue.getTop();
 };
+
+let nums = [3, 2, 1, 5, 6, 4],
+	k = 2;
+// (nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]), (k = 4);
+console.log(findKthLargest(nums, k));
