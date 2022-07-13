3aafe0a9300cbb25659b60a1560b06c00838bdef
zhengpj <598359997@qq.com>
2022-07-13 10:36:26 +0800

    update 133

--- search/133.CloneGraph.js
+++ search/133.CloneGraph.js
@@ -1,3 +1,13 @@
+/**
+ * https://leetcode.com/problems/clone-graph/
+ * Constraints:
+ * The number of nodes in the graph is in the range [0, 100].
+ * 1 <= Node.val <= 100
+ * Node.val is unique for each node.
+ * There are no repeated edges and no self-loops in the graph.
+ * The Graph is connected and all nodes can be visited starting from the given node.
+ */
+
 // Definition for a Node.
 function Node(val, neighbors) {
 	this.val = val === undefined ? 0 : val;
@@ -33,4 +43,51 @@ var cloneGraph = function (node) {
 	};
 
 	return dfs(node);
+};
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph2 = function (node) {
+	if (!node) {
+		return node;
+	}
+	let build = (node, map) => {
+		if (!node || map.has(node.val)) {
+			return;
+		}
+		let cloned = new Node(node.val);
+		map.set(node.val, cloned);
+
+		if (node.neighbors) {
+			for (let n of node.neighbors) {
+				build(n, map);
+			}
+		}
+	};
+
+	let connect = (node, map, visited) => {
+		if (!node || visited.has(node.val)) {
+			return;
+		}
+		visited.set(node.val, true);
+		let cloned = map.get(node.val);
+		if (!cloned) {
+			return;
+		}
+		if (node.neighbors) {
+			for (let n of node.neighbors) {
+				cloned.neighbors.push(map.get(n.val));
+				connect(n, map, visited);
+			}
+		}
+	};
+
+	let map = new Map();
+	build(node, map);
+	let visited = new Map();
+	connect(node, map, visited);
+
+	return map.get(node.val);
 };
